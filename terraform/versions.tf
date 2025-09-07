0476cfb8d727774bb20557ef8d1616c30af3a431
sawurora <sawurora@proton.me>
2025-09-07 17:22:43 -0300

    feat: add backend state

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,4 +1,10 @@
 terraform {
+  backend "s3" {
+    bucket = "aquarela-terraform-state"
+    key = "aquarela-devops/terraform.tfstate"
+    region = "us-east-1"
+  }
+
   required_version = ">= 1.8.0"
 
   required_providers {
